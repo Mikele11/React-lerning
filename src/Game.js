e15cf9ed779a51f3ca6642f5830298fc7b2b03ec
Mikele11 <mihailo_rlyash@mail.ru>
2018-09-18 16:39:46 +0300

    change state to click

--- src/Game.js
+++ src/Game.js
@@ -8,13 +8,16 @@ class Square extends React.Component {
         };
     }    
     render() {
-      return (
-        <button className="square" onClick={() => alert('click')}>
-            {this.props.value}
-        </button>
-      );
+        return (
+          <button
+            className="square"
+            onClick={() => this.setState({value: 'X'})}
+          >
+            {this.state.value}
+          </button>
+        );
+      }
     }
-  }
   
   class Board extends React.Component {
     renderSquare(i) {
