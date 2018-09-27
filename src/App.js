049f259c0662006c44f6ff8cc5c3fc96c594a7c7
Andriy Leliv <andrii.leliv@gmail.com>
2018-09-27 09:02:40 +0200

    Revert "add handle button to Person"
    This reverts commit 95a3c2885c7cf2be4a159d323a4f942a42473bf9.

--- src/App.js
+++ src/App.js
@@ -10,17 +10,12 @@ class App extends Component {
       { name: 'Stephanie', age: 26 }
     ]
   }
-
-  switchNameHandler = () => {
-    console.log('Was clicked!');
-  }
-
   render() {
     return (
       <div className="App">
         <h1>Hi, I am React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <button>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >My Hobies</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
