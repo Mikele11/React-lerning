def0f31fb47c5a3d3e13bfd160dc7555422444c8
Andriy Leliv <andrii.leliv@gmail.com>
2018-09-27 09:02:40 +0200

    Revert "add props.children to Person"
    This reverts commit 02a530a0088681b2bacd9f1751137bc4ae28898b.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,5 @@
 import React from 'react'
 const person = (props) =>{
-    return (
-        <div>
-          <p>I am {props.name} a Person and I am {props.age} years old!</p>
-          <p>{props.children}</p>
-        </div>   
-    )
-  }
+    return <p>I am {props.name} a Person and I am {props.age} years old!</p>
+}
 export default person;
