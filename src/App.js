fc0b2b630927498af76bd9a3500d646b0ff1abd6
Andriy Leliv <andrii.leliv@gmail.com>
2018-09-27 09:02:40 +0200

    Revert "dynamic content"
    This reverts commit 690882b0a35311c13b616775dacece88f44c5c20.

--- src/App.js
+++ src/App.js
@@ -51,26 +51,6 @@ class App extends Component {
       cursor: 'pointer'
     }
 
-    let persons = null;
-    if (this.state.showPersons) {
-     persons = (
-        <div>
-          <Person 
-            name={this.state.persons[0].name} 
-            age={this.state.persons[0].age} />
-          <Person 
-            name={this.state.persons[1].name} 
-            age={this.state.persons[1].age} 
-            click={this.switchNameHandler.bind(this, 'Max!')} 
-            changed={this.nameChangedHandler}>My Hobies</Person>
-          <Person 
-            name={this.state.persons[2].name} 
-            age={this.state.persons[2].age} />
-        </div>
-      );
-    }
-
-
     return (
       <div className="App">
         <h1>Hi, I am React App</h1>
@@ -78,7 +58,22 @@ class App extends Component {
         <button
           style={style} 
           onClick={this.tooglePersonsHandler}>Switch Name</button>
-          {persons}  
+          {
+            this.state.showPersons === true ?
+              <div>
+                <Person 
+                  name={this.state.persons[0].name} 
+                  age={this.state.persons[0].age} />
+                <Person 
+                  name={this.state.persons[1].name} 
+                  age={this.state.persons[1].age} 
+                  click={this.switchNameHandler.bind(this, 'Max!')} 
+                  changed={this.nameChangedHandler}>My Hobies</Person>
+                <Person 
+                  name={this.state.persons[2].name} 
+                  age={this.state.persons[2].age} />
+              </div>:null
+          }   
       </div>
     );
   }
