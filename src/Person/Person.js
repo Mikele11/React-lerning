ff5cbd22a818e4a4949f68fd8b4aa505049cf165
Andriy Leliv <andrii.leliv@gmail.com>
2018-09-27 09:02:40 +0200

    Revert "add css to Person"
    This reverts commit 7df31978188220f5d87a5363bdc98425179e5d21.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import './Person.css';
-
 const person = (props) =>{
     return (
-        <div className="Person">
+        <div>
           <p onClick={props.click}>I am {props.name} a Person and I am {props.age} years old!</p>
           <p>{props.children}</p>
           <input type="text" onChange={props.changed} value={props.name} />
