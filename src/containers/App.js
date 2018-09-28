82948e800051cd7faefe654f99154dcb1cd3964d
Mikele11 <mihailo_rlyash@mail.ru>
2018-09-28 05:48:15 +0300

    creation lifecycle of components

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,16 +4,26 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: '1', name: 'Max', age: 28 },
-      { id: '2', name: 'Menu', age: 29 },
-      { id: '3', name: 'Stephanie', age: 26 }
-    ],
-    otherState: 'some value',
-    showPersons: false
+  constructor(props){
+    super(props)
+    console.log('[App js] inside Constructor',props);
+    this.state = {
+      persons: [
+        { id: '1', name: 'Max', age: 28 },
+        { id: '2', name: 'Menu', age: 29 },
+        { id: '3', name: 'Stephanie', age: 26 }
+      ],
+      otherState: 'some value',
+      showPersons: false
+    }
   }
 
+  componentWillMount(){
+    console.log('[App js] inside componentWillMount');
+  }
+  componentDidMount(){
+    console.log('[App js] inside componentDidMount');
+  }
   switchNameHandler = (newName) => {
     this.setState({
       persons: [
@@ -51,6 +61,7 @@ class App extends Component {
   }
 
   render() {
+    console.log('[App js] inside render');
     let persons = null;
     if (this.state.showPersons) {
      persons = <Persons 
