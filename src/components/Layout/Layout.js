881e17fe0f1ab9a6f7a9eaad5f84f13a326933a2
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-03 04:54:31 +0300

    add sideDrawer and fix css image

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Aux1 from '../../hoc/Aux1'
 import classes from './Layout.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = ( props ) => (
     <Aux1>
         <Toolbar />
+        <SideDrawer />
         <main className = { classes.Content } >
             { props.children }
         </main>
