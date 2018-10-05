ebc1c224ad70473792c19c8fdefe644a1b641f84
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-05 09:19:37 +0300

    add to redirect routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-//import axios from 'axios'
+// import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
+
 import './Blog.css';
-import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
-import { Route, NavLink, Switch } from 'react-router-dom'
-import FullPost from './FullPost/FullPost'
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-    render () {  
+    render () {
         return (
             <div className="Blog">
                 <header>
                     <nav>
                         <ul>
-                            <li><NavLink 
-                            to='/'
-                            exact
-                            activeClassName='my-active'
-                            activeStyle={{
-                                color: '#fa923f',
-                                testDecoration: 'underline'
-                            }}>Posts</NavLink></li>
+                            <li><NavLink
+                                to="/posts/"
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#fa923f',
+                                    textDecoration: 'underline'
+                                }}>Posts</NavLink></li>
                             <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
@@ -29,13 +29,13 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                {/*
-                <Route path='/' exact render={()=><h1>Home</h1>} />
-                <Route path='/' render={()=><h1>Home 2</h1>} />
-                */}
+                {/* <Route path="/" exact render={() => <h1>Home</h1>} />
+                <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path='/new-post' component={NewPost} />
-                    <Route path='/' component={Posts} />
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                    <Redirect from="/" to="/posts" />
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
         );
