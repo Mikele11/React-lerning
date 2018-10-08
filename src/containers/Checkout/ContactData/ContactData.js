c2c0b6aa8f3ccc6bf590902603459058ab261c2c
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-08 11:11:51 +0300

    fixes to props and outputing

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,21 +8,21 @@ class ContactData extends Component {
     state = {
         name: '',
         email: '',
-        adress: {
+        address: {
             street: '',
             postalCode: ''
         },
         loading: false
     }
 
-    orderHandler = (event) => {
+    orderHandler = ( event ) => {
         event.preventDefault();
-        this.setState({ loading: true });
+        this.setState( { loading: true } );
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
             customer: {
-                name: 'Max Shcwarxmuller',
+                name: 'Max Schwarzmüller',
                 address: {
                     street: 'Teststreet 1',
                     zipCode: '41351',
@@ -32,14 +32,14 @@ class ContactData extends Component {
             },
             deliveryMethod: 'fastest'
         }
-        axios.post('/orders.json', order)
-            .then(response =>{
-                this.setState({ loading: false });
+        axios.post( '/orders.json', order )
+            .then( response => {
+                this.setState( { loading: false } );
                 this.props.history.push('/');
-            })
-            .catch(error =>{
-                this.setState({ loading: false });
-            })
+            } )
+            .catch( error => {
+                this.setState( { loading: false } );
+            } );
     }
 
     render () {
@@ -52,8 +52,8 @@ class ContactData extends Component {
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
-        if (this.state.loading) {
-            form = <Spinner />
+        if ( this.state.loading ) {
+            form = <Spinner />;
         }
         return (
             <div className={classes.ContactData}>
@@ -62,7 +62,6 @@ class ContactData extends Component {
             </div>
         );
     }
-
 }
 
 export default ContactData;
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -8,10 +8,11 @@ class Orders extends Component {
         orders: [],
         loading: true
     }
-    componentDidMount () {
+
+    componentDidMount() {
         axios.get('/orders.json')
-            .then(res=>{
-                const fetchedOrders =[];
+            .then(res => {
+                const fetchedOrders = [];
                 for (let key in res.data) {
                     fetchedOrders.push({
                         ...res.data[key],
@@ -20,19 +21,23 @@ class Orders extends Component {
                 }
                 this.setState({loading: false, orders: fetchedOrders});
             })
-            .catch(err=>{
+            .catch(err => {
                 this.setState({loading: false});
-            })
+            });
     }
+
     render () {
         return (
             <div>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => (
+                    <Order 
+                        key={order.id}
+                        ingredients={order.ingredients}
+                        price={order.price} />
+                ))}
             </div>
         );
     }
 }
-
 
 export default withErrorHandler(Orders, axios);
