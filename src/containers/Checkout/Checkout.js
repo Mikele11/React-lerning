73c6e1b4d249dc207c8b54b9cb4506871369393c
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-08 06:12:04 +0300

    navigation to contact data

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
     state = {
@@ -24,7 +26,7 @@ class Checkout extends Component {
         this.props.history.goBack();
     }
 
-    checkoutContinuedHandletr = () => {
+    checkoutContinuedHandler = () => {
         this.props.history.replace('/checkout/contact-data')
     }
 
@@ -35,6 +37,9 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler} />
+                <Route 
+                    path={this.props.match.path + '/contact-data'} 
+                    component={ContactData} />
             </div>
         );
     }
