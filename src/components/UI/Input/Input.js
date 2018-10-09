6c2bda25a4725e9bc7a33776ba045d264a7e6af3
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-09 08:45:50 +0300

    handlinf input change

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -8,19 +8,22 @@ const input = (props) => {
             inputElement = <input 
                 {...props.elementConfig} 
                 className={classes.InputElement}
-                value={props.value} />;
+                value={props.value}
+                onChange={props.changed} />;
             break;
         case ('textarea'):
             inputElement = <textarea 
                 {...props.elementConfig} 
                 className={classes.InputElement}
-                value={props.value} />;
+                value={props.value}
+                onChange={props.changed} />;
             break; 
         case ('select'):
             inputElement = (
                 <select
                     className={classes.InputElement}
-                    value={props.value}>
+                    value={props.value}
+                    onChange={props.changed}>
                     {props.elementConfig.options.map(option => (
                         <option key={option.value} value={option.value}>
                             {option.displayValue}
@@ -33,7 +36,8 @@ const input = (props) => {
             inputElement = <input 
                 {...props.elementConfig} 
                 className={classes.InputElement}
-                value={props.value} />;      
+                value={props.value}
+                onChange={props.changed} />;      
     }
     return (
         <div className={classes.Input}>
