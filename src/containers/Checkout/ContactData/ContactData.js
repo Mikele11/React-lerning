8b1c3325144459c04dd70ecc6501040930954c56
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-09 09:44:44 +0300

    fix logic validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -107,17 +107,17 @@ class ContactData extends Component {
     }
 
     checkValidity( value, rules ) {
-        let isValid = false;
+        let isValid = true;
         if (rules.required) {
-            isValid = value.trim() !== ''
+            isValid = value.trim() !== '' && isValid;
         }
 
         if (rules.minLength) {
-            isValid = value.value >= rules.minLength
+            isValid = value.length >= rules.minLength && isValid;
         }
-        
+
         if (rules.maxLength) {
-            isValid = value.value <= rules.maxLength
+            isValid = value.length <= rules.maxLength && isValid;
         }
 
 
