4c572d450245145b0a92d28c85df6029e00011f5
Mikele11 <mihailo_rlyash@mail.ru>
2018-10-19 03:34:01 +0300

    start page

---
+++ .next/dist/pages/index.js
@@ -0,0 +1,31 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "C:\\Learn\\React\\udemy\\19\\udemy\\pages\\index.js?entry";
+
+
+var indexPage = function indexPage() {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    }
+  }, _react2.default.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    }
+  }, "The Main Page"));
+};
+
+exports.default = indexPage;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxpbmRleC5qcyJdLCJuYW1lcyI6WyJSZWFjdCIsImluZGV4UGFnZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7Ozs7Ozs7O0FBRVAsSUFBTSxZQUFZLFNBQVosQUFBWSxZQUFBO3lCQUNoQixjQUFBOztnQkFBQTtrQkFBQSxBQUNFO0FBREY7QUFBQSxHQUFBLGtCQUNFLGNBQUE7O2dCQUFBO2tCQUFBO0FBQUE7QUFBQSxLQUZjLEFBQ2hCLEFBQ0U7QUFGSixBQU1BOztrQkFBQSxBQUFlIiwiZmlsZSI6ImluZGV4LmpzP2VudHJ5Iiwic291cmNlUm9vdCI6IkM6L0xlYXJuL1JlYWN0L3VkZW15LzE5L3VkZW15In0=
--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # React-lerning
-Lerning React branch
+
+## Changes
+
+#### 19.10.2018 — next
+
+
+## Learned
+
+#### 19.10.2018 — next
+
+
+### Definitions
+
+#### 19.10.2018 — next
+
+
+## Questions
+
+#### 19.10.2018 — next
+
+**There were no questions on this stage**
+
+#### 19.10.2018 — next
+
+**I don't quite understand how use interceptors**
+
+
